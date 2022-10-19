ca3075c37bfb9068e3ccb114b88c3f0a5f92aee4
pancho <pancho@localhost.localdomain>
2022-10-19 13:00:33 -0300

    update

--- util.py
+++ util.py
@@ -6,41 +6,20 @@ import json
 import os
 import random
 import string
-from time import sleep
 
 
 def fwrite(file, date):
-    if not date:
-        date = "None"
-    if date == "void":
-        date = ""
-
-    try:
-        f = open(file, "w")
-        f.write(date)
-        f.close()
-    except:
-        None
+    f = open(file, "w")
+    f.write(date)
+    f.close()
 
 
 def fread(file):
-    reading = "0"
-    for _ in range(100):
-        if os.path.isfile(file):
-            f = open(file, "r")
-            reading = str(f.read().strip())
-            f.close()
+    f = open(file, "r")
+    readed = str(f.read().strip())
+    f.close()
 
-            if reading:
-                break
-
-        else:
-            break
-
-        sleep(0.07)
-
-    return reading
-
+    return readed
 
 def jread(file):
     return json.loads(fread(file))
